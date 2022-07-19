01f452a7b8245d968faef2b892b0995b9d0f6013
Roman Barakhvostov <rbarakhvostov@gmail.com>
2022-07-19 17:18:45 +0300

    add stuff

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import { createServer } from '@graphql-yoga/node'
+
+const messages = []
+
+const typeDefs = `
+  type Message {
+    id: ID!
+    user: String!
+    content: String!
+  }
+
+  type Query {
+    messages: [Message!],
+  }
+
+  type Mutation {
+    postMessage(user: String!, content: String!): ID!
+  }
+`
+const resolvers = {
+  Query: {
+    messages: () => messages,
+  },
+
+  Mutation: {
+    postMessage: (parent, {user, content}) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        content
+      })
+
+      return id
+    }
+  }
+}
+
+const server = createServer({
+  schema: {
+    typeDefs,
+    resolvers
+  }
+})
+
+server.start()
